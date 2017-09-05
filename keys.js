897a88c71741b0aa8a7124bffcfc87bd976ac5b5
Manisha Pawar <manishapawar.net@gmail.com>
2017-09-04 23:17:30 -0500

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,1 +1,8 @@
+console.log('this is loaded');
 
+exports.twitterKeys = {
+  consumer_key: '5rdDoLS55hm8jSmQHymPc3KjT',
+  consumer_secret: 'DHhRmllKllUXN1kAc1YjGIhPEdMJvxPZM1Rt5BTNEAjG5MRpuC',
+  access_token_key: '904918043356213248-K7iQwVh4H8ChoDtgArcbfPy9HbjBl57',
+  access_token_secret: 'DcLvu6J2fVSNgmZhwbYfOHIcs58OPt3A7g4uCKN1m5PeA',
+}
