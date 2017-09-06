3acdaa8ca283050231b52d2d064754be491aa903
manishap1 <manishapawar.net@gmail.com>
2017-09-06 08:09:35 -0500

    liri updated

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '904918043356213248-K7iQwVh4H8ChoDtgArcbfPy9HbjBl57',
   access_token_secret: 'DcLvu6J2fVSNgmZhwbYfOHIcs58OPt3A7g4uCKN1m5PeA',
 }
+
+exports.spotifyKeys={
+	client_id:"16707a80bdea48e5a88d4f878eaf0e92",
+	client_secret:"38663f774c6c4f3cae6d19f1c081a251"
+}
